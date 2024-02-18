28057684391271373fcbef9852fc5bfdd6da84e8
saikirananumalla <saikirananumalla@gmail.com>
2024-02-17 22:21:43 -0500

    class activity react - 14 feb

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,17 @@
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Profile from './components/profile'
+import Form from "@/pages/components/updObjects";
+import FeedbackForm from "@/pages/qcomps/thankYouCrash";
+import Counter from "@/pages/components/queueUpdates";
+import RequestTracker from "@/pages/qcomps/shoppingCart";
+import Scoreboard from "@/pages/qcomps/updObjectsForm";
+import List from "@/pages/components/artistsArr";
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        <List />
     </div>
   )
 }

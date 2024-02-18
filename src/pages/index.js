2e6ae063f2a860e6df4ac44abb34e8183cbfdf61
saikirananumalla <saikirananumalla@gmail.com>
2024-02-17 22:56:33 -0500

    class activity react - 14 feb

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,13 @@ import FeedbackForm from "@/pages/qcomps/thankYouCrash";
 import Counter from "@/pages/components/queueUpdates";
 import RequestTracker from "@/pages/qcomps/shoppingCart";
 import Scoreboard from "@/pages/qcomps/updObjectsForm";
-import List from "@/pages/components/artistsArr";
+import List from "@/pages/qcomps/artistsRemoveArr";
+import BucketList from "@/pages/qcomps/arrObj";
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <List />
+        <BucketList />
     </div>
   )
 }

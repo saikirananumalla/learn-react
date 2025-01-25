55e3996990d600def5d85d8f358309e1d5076272
saikirananumalla <saikirananumalla@gmail.com>
2025-01-24 23:46:00 -0500

    fix gallery,list_items and recipes

--- src/pages/qcomps/recipes.tsx
+++ src/pages/qcomps/recipes.tsx
@@ -12,11 +12,29 @@ export const recipes = [{
   ingredients: new Set(['chickpeas', 'olive oil', 'garlic cloves', 'lemon', 'tahini'])
 }];
 
+
+function Recipe({ingredients} : {ingredients: Set<String>}) {
+  const listItems = Array.from(ingredients).map((ingredient) => (
+    <li>{ingredient}</li>
+  ));
+  return <ul>{listItems}</ul>;
+}
+
 export default function RecipeList() {
+
+  const listRecipes = recipes.map(recipe => 
+    <li key = {recipe.id}>
+      <h2>{recipe.name}</h2>
+      <Recipe ingredients={recipe.ingredients}></Recipe>
+    </li>
+  )
+  
   return (
     <div>
       <h1>Recipes</h1>
-      {}
+      <ul>{listRecipes}</ul>
     </div>
   );
+
+  
 }
---
+++ src/types/scientist.ts
@@ -0,0 +1,4 @@
+export interface ProfileProps {
+    scientist: {name: string, imageId: string, profession: string, awards: string, noOfAwards: string, discovery: string}, 
+    size?: number
+}
